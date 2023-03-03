ea050afe65c19c3e379e417ef735adb94b992817
Valentyna Kondrashenko <oreshkav@gmail.com>
2023-03-03 18:53:52 +0100

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.print("Enter the month: "); // Введите название месяца
+      System.out.println("The month '%s' has ordinal number %d%n");
+
+
+  }
+}
